837278223b1d90858dd349bac0bb51ca1d4a5e64
Luis Morales <lemorales9@gmail.com>
2013-04-19 17:24:21 -0300

    Borre la carpeta 'controlador' duplicada

--- controlador/conexion.php
+++ controlador/conexion.php
@@ -3,7 +3,7 @@
 
 
 function conectar_bd() {
-    $conexion = @mysql_connect("localhost:3307", "root", "root") or die("Fallo en el establecimiento de la conexion !");
+    $conexion = @mysql_connect("localhost", "root", "root") or die("Fallo en el establecimiento de la conexion !");
     @mysql_select_db("panaderia") or die("No se encuentra la Base de Datos !");
     return $conexion;
 }
